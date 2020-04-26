12e8573950bde59a48c04586729ddca805d0fec5
Loskir <kl@sinker.ru>
2020-04-26 13:41:34 +0300

    :tada: Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# 50 Lines (Web version)
+
+## Original idea by u-ndefine: [gist](https://gist.github.com/u-ndefine/8e4bc21be4275f87fefe7b2a68487161)
+![Demo](demo.png)
+
+[Link](https://loskir.github.io/50-lines)
+
+Telegram bot: [@Lines50Bot](https://tttttt.me/Lines50Bot)
+
---
+++ main.js
@@ -0,0 +1,80 @@
+const sourceCanvas = document.getElementById('source')
+const resultCanvas = document.getElementById('result')
+const fileInput = document.getElementById('file-input')
+
+const sourceCtx = sourceCanvas.getContext('2d')
+const resultCtx = resultCanvas.getContext('2d')
+
+const HEIGHT = 700
+
+let imageWidth = 0
+let imageHeight = 0
+
+const updateCanvasSize = () => {
+  sourceCanvas.width = imageWidth
+  sourceCanvas.height = imageHeight
+  resultCanvas.width = imageWidth
+  resultCanvas.height = imageHeight
+}
+
+const readAsDataUrl = (file) => new Promise((resolve) => {
+  const reader = new FileReader()
+  reader.onload = (e) => resolve(e.target.result)
+  reader.readAsDataURL(file)
+})
+
+const loadImage = (src) => new Promise((resolve) => {
+  const img = new Image()
+  img.onload = () => resolve(img)
+  img.src = src
+})
+
+fileInput.addEventListener('input', async (e) => {
+  console.log(e)
+  if (fileInput.files && fileInput.files[0]) {
+    console.log(fileInput.files[0])
+    const dataUrl = await readAsDataUrl(fileInput.files[0])
+    const img = await loadImage(dataUrl)
+
+    imageWidth = Math.floor(img.width * HEIGHT / img.height)
+    imageHeight = HEIGHT
+
+    updateCanvasSize()
+
+    sourceCtx.drawImage(img, 0, 0, imageWidth, imageHeight)
+
+    const imgd = sourceCtx.getImageData(0, 0, imageWidth, imageHeight)
+    const pix = imgd.data
+    const n = pix.length
+
+    for (let i = 0; i < n; i += 4) {
+      const grayscale = pix[i + 3] === 0 ? 255 : pix[i] * .3 + pix[i + 1] * .59 + pix[i + 2] * .11
+      pix[i] = grayscale
+      pix[i + 1] = grayscale
+      pix[i + 2] = grayscale
+      pix[i + 3] = 255
+    }
+
+    for (let y = 0; y < 50; ++y) {
+      resultCtx.beginPath()
+      resultCtx.lineWidth = 2
+      resultCtx.lineJoin = 'round'
+
+      let l = 0;
+
+      for (let x = 0; x < imageWidth; ++x) {
+        const c = pix[((y * imageHeight / 50) * imageWidth + x)*4]
+
+        l += (255 - c) / 255
+
+        const m = (255 - c) / 255
+
+        resultCtx.lineTo(
+          x,
+          (y + 0.5) * imageHeight / 50 + Math.sin(l * Math.PI / 2) * 5 * m
+        )
+      }
+      resultCtx.stroke()
+    }
+  }
+})
